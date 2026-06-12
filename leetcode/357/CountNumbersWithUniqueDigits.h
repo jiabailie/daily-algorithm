e97319d8b5400de22c1f238b253ff49260d9de14
Ruiguo Yang <spotlight_yrg@hotmail.com>
2026-06-12 15:05:24 +0800

    Add practice of 2026/06/12

---
+++ leetcode/357/CountNumbersWithUniqueDigits.h
@@ -0,0 +1,18 @@
+//
+// Created by Ruiguo Yang on 2026/6/12.
+//
+
+#ifndef ALGORITHM_COUNTNUMBERSWITHUNIQUEDIGITS_H
+#define ALGORITHM_COUNTNUMBERSWITHUNIQUEDIGITS_H
+
+#include "../Base.h"
+
+namespace leetcode {
+    class CountNumbersWithUniqueDigits: public Base {
+    public:
+        int countNumbersWithUniqueDigits(int n);
+        void test() override;
+    };
+}
+
+#endif //ALGORITHM_COUNTNUMBERSWITHUNIQUEDIGITS_H
