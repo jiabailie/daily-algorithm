a535911f67dd21a72f9dac59adbcad0bec647ce0
Ruiguo Yang <spotlight_yrg@hotmail.com>
2026-06-16 09:55:58 +0800

    Add practice of 2026/06/16 (#71)

---
+++ leetcode/3612/ProcessStringWithSepcialOperationsI.h
@@ -0,0 +1,20 @@
+//
+// Created by Ruiguo Yang on 2026/6/16.
+//
+
+#ifndef ALGORITHM_PROCESSSTRINGWITHSEPCIALOPERATIONSI_H
+#define ALGORITHM_PROCESSSTRINGWITHSEPCIALOPERATIONSI_H
+
+#include "../Base.h"
+
+namespace leetcode {
+    class ProcessStringWithSepcialOperationsI: public Base {
+    public:
+        std::string processStr(std::string s);
+        void test() override;
+    };
+}
+
+
+
+#endif //ALGORITHM_PROCESSSTRINGWITHSEPCIALOPERATIONSI_H
