910034bfb02e37933f361888a803aca7f0a95035
Ruiguo Yang <spotlight_yrg@hotmail.com>
2026-06-17 10:00:49 +0800

    Add practice of 2026/06/17 (#72)
    * Add practice of 2026/06/17
    
    * Fix macro error
    
    ---------
    
    Co-authored-by: ruiguo <ruiguo@ruiguos-MacBook-Pro.local>

---
+++ leetcode/3614/ProcessStringWithSpecialOperationsII.cpp
@@ -0,0 +1,57 @@
+//
+// Created by ruiguo on 2026/6/17.
+//
+
+#include "ProcessStringWithSpecialOperationsII.h"
+
+#include <iostream>
+#include <ostream>
+
+namespace leetcode {
+    char ProcessStringWithSpecialOperationsII::processStr(std::string s, long long k) {
+        int n = s.size();
+        std::vector<long long> size(n);
+        long long sz = 0;
+        for (int i = 0; i < n; ++i) {
+            char c = s[i];
+            if (c == '*') {
+                sz = std::max(sz - 1, 0LL);
+            } else if (c == '#') {
+                sz *= 2;
+            } else if (c != '%') {
+                sz++;
+            }
+            size[i] = sz;
+        }
+
+        if (k >= size[n - 1]) {
+            return '.';
+        }
+
+        for (int i = n - 1; ; --i) {
+            char c = s[i];
+            sz = size[i];
+            if (c == '#') {
+                if (k >= sz / 2) {
+                    k -= sz / 2;
+                }
+            } else if (c == '%') {
+                k = sz - 1 - k;
+            } else if (c != '*' && k == sz - 1) {
+                return c;
+            }
+        }
+    }
+
+    void ProcessStringWithSpecialOperationsII::test() {
+        std::vector<std::string> s {"a#b%*", "cd%#*#","z*#"};
+        std::vector<long long> k {1, 3, 0};
+        std::vector<char> expect {'a', 'd', '.'};
+        for (int i = 0; i < expect.size(); ++i) {
+            char actual = processStr(s[i], k[i]);
+            std::cout << (actual == expect[i] ? "PASS" : "FAIL") << std::endl;
+        }
+    }
+}
+
+REGISTER_LEETCODE_SOLUTION("3614", leetcode::ProcessStringWithSpecialOperationsII);
