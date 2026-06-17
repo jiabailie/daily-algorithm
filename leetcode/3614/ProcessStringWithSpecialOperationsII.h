071a15d8a6615bdbd6ecca0e27e4205b91d00a0d
ruiguo <ruiguo@ruiguos-MacBook-Pro.local>
2026-06-17 09:56:50 +0800

    Add practice of 2026/06/17

---
+++ leetcode/3614/ProcessStringWithSpecialOperationsII.h
@@ -0,0 +1,19 @@
+//
+// Created by ruiguo on 2026/6/17.
+//
+
+#ifndef ALGORITHM_PROCESSSTRINGWITHSPECIALOPERATIONSII_H
+#define ALGORITHM_PROCESSSTRINGWITHSPECIALOPERATIONSII_H
+
+#include "../Base.h"
+
+namespace leetcode {
+    class ProcessStringWithSpecialOperationsII: public Base {
+    public:
+        char processStr(std::string s, long long k);
+        void test() override;
+    };
+}
+
+
+#endif //ALGORITHM_PROCESSSTRINGWITHSPECIALOPERATIONSII_H
