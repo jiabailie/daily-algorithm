0ebd77b05c2cbcd42c41a1f6b7fe96646326b298
Ruiguo Yang <spotlight_yrg@hotmail.com>
2026-07-21 13:38:33 +0800

    Add practice of 2026/07/21 (#107)

---
+++ leetcode/3499/MaximizeActiveSectionWithTradeI.h
@@ -0,0 +1,18 @@
+//
+// Created by ruiguo on 2026/7/21.
+//
+
+#ifndef ALGORITHM_MAXIMIZEACTIVESECTIONWITHTRADEI_H
+#define ALGORITHM_MAXIMIZEACTIVESECTIONWITHTRADEI_H
+
+#include "../Base.h"
+
+namespace leetcode {
+    class MaximizeActiveSectionWithTradeI: public Base {
+    public:
+        int maxActiveSectionsAfterTrade(std::string s);
+        void test() override;
+    };
+}
+
+#endif //ALGORITHM_MAXIMIZEACTIVESECTIONWITHTRADEI_H
