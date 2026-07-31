c8d7589dfb62b272d510a7156085b4104c4e59fe
Ruiguo Yang <spotlight_yrg@hotmail.com>
2026-07-31 09:49:49 +0800

    Add practice of 2026/07/31 (#113)
    * Add practice of 2026/07/31
    
    * Fix CI error

---
+++ leetcode/3016/MinimumNumberOfPushesToTypeWordII.h
@@ -0,0 +1,18 @@
+//
+// Created by ruiguo on 2026/7/31.
+//
+
+#ifndef ALGORITHM_MINIMUMNUMBEROFPUSHESTOTYPEWORDII_H
+#define ALGORITHM_MINIMUMNUMBEROFPUSHESTOTYPEWORDII_H
+
+#include "../Base.h"
+
+namespace leetcode {
+    class MinimumNumberOfPushesToTypeWordII: public Base {
+    public:
+        int minimumPushes(std::string word);
+        void test() override;
+    };
+}
+
+#endif //ALGORITHM_MINIMUMNUMBEROFPUSHESTOTYPEWORDII_H
