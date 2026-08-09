1b87f73710c0e697b1781adfa59cd7e25afbfe9b
Ruiguo Yang <spotlight_yrg@hotmail.com>
2026-08-09 22:21:12 +0800

    Add practice of 2026/08/09

---
+++ leetcode/1140/StoneGameII.h
@@ -0,0 +1,18 @@
+//
+// Created by Ruiguo on 9/8/2026.
+//
+
+#ifndef ALGORITHM_STONEGAMEII_H
+#define ALGORITHM_STONEGAMEII_H
+
+#include "../Base.h"
+
+namespace leetcode {
+    class StoneGameII: public Base {
+    public:
+        int stoneGameII(std::vector<int>& piles);
+        void test() override;
+    };
+}
+
+#endif //ALGORITHM_STONEGAMEII_H
