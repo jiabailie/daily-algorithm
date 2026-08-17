cbe7492108f7bc70d94fecb271ad6b986578fd69
Ruiguo Yang <spotlight_yrg@hotmail.com>
2026-08-17 10:13:46 +0800

    Add practice of 2026/08/16

---
+++ leetcode/1563/StoneGameV.h
@@ -0,0 +1,18 @@
+//
+// Created by ruiguo on 2026/8/17.
+//
+
+#ifndef ALGORITHM_STONEGAMEV_H
+#define ALGORITHM_STONEGAMEV_H
+
+#include "../Base.h"
+
+namespace leetcode {
+    class StoneGameV: public Base {
+    public:
+        int stoneGameV(std::vector<int>& stoneValue);
+        void test() override;
+    };
+}
+
+#endif //ALGORITHM_STONEGAMEV_H
