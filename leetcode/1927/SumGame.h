49856a8d6cc8bb0d0f167676a7f49c8c2632c43a
Ruiguo Yang <spotlight_yrg@hotmail.com>
2026-08-23 16:01:42 +0800

    Add practice of 2026/08/23

---
+++ leetcode/1927/SumGame.h
@@ -0,0 +1,18 @@
+//
+// Created by Ruiguo on 23/8/2026.
+//
+
+#ifndef ALGORITHM_SUMGAME_H
+#define ALGORITHM_SUMGAME_H
+
+#include "../Base.h"
+
+namespace leetcode {
+    class SumGame: public Base {
+    public:
+        bool sumGame(std::string num);
+        void test() override;
+    };
+}
+
+#endif //ALGORITHM_SUMGAME_H
